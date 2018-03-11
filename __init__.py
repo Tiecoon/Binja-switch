2909d49b1d9e13e95cdc9ffe4d0ac5a318cf5296
Max <mx@icj.me>
2018-03-11 00:57:23 -0500

    Fixed issues with BinaryView et alii
    Segments now work properly. Need to make sections now, and then parse
    mod0.

--- __init__.py
+++ __init__.py
@@ -39,10 +39,25 @@ class SwitchExecutableView(BinaryView):
 	should_check_hash = False
 
 	def __init__(self, data):
-		BinaryView.__init__(self, file_metadata=data.file, parent_view=data)
-		self.raw = data
-		self.platform = Architecture['aarch64'].standalone_platform 
 
+		self.sections = []
+		self.should_check_hash = False
+
+		self.parseFile(data)
+		print(self.sections)
+
+		decompressedData = self.sections[0].sectionData + self.sections[1].sectionData + self.sections[2].sectionData
+
+		fake_parent = BinaryView.new(data=decompressedData)
+		BinaryView.__init__(self, file_metadata=data.file, parent_view=fake_parent)
+		self.data = data
+
+		self.platform = Architecture['aarch64'].standalone_platform 
+		self.arch = Architecture['aarch64']
+
+		self.init_real()
+
+	def parseFile(self, data):
 		header = data[0:0x100]
 
 		flags = struct.unpack("<I", header[0xC:0xC + 4])[0]
@@ -63,31 +78,21 @@ class SwitchExecutableView(BinaryView):
 		mod0Addr = unpack32(self.sections[0].sectionData[4:8])
 		print('MOD0: ' + str(mod0Addr))
 
-		__data = self.sections[0].sectionData + self.sections[1].sectionData + self.sections[2].sectionData
-		f = open("/tmp/bbbb", "w+")
-		f.write(__data)
-		f.close()
-		
-		print(self.sections)
-		self.init_real()
-
 	@classmethod
 	def is_valid_for_data(self, data):
 		if data[0:4] == 'NSO0':
 			return True
 		return False
-
 	def init_common(self):
 		pass
 
-	def perform_read(self, addr, length):
-		return self.sections[0].sectionData[addr: addr +length]
-
 	def init_real(self):
 		sectionStrings = ["text", "data", "rodata"]
-		sectionSemantics = [SectionSemantics.ReadOnlyCodeSectionSemantics, SectionSemantics.ReadWriteDataSectionSemantics, SectionSemantics.ReadOnlyDataSectionSemantics]
+		segflags = [SegmentFlag.SegmentContainsCode | SegmentFlag.SegmentExecutable | SegmentFlag.SegmentReadable, SegmentFlag.SegmentContainsData | SegmentFlag.SegmentWritable | SegmentFlag.SegmentReadable, SegmentFlag.SegmentContainsData | SegmentFlag.SegmentDenyWrite | SegmentFlag.SegmentReadable ]
+		len_ = 0
 		for i in range(3):
-			self.add_user_section(sectionStrings[i], self.sections[i].memoryOffset, self.sections[i].decompressedSize, sectionSemantics[i])
+			self.add_user_segment(self.sections[i].memoryOffset, self.sections[i].decompressedSize, len_, self.sections[i].decompressedSize, segflags[i])
+			len_ += self.sections[i].decompressedSize
 		self.add_entry_point(self.sections[0].memoryOffset)
 
 	def perform_is_offset_readable(self, offt):
